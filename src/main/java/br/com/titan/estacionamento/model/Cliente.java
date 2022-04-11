bab728d44f6732c4bdd5c23939ecace53198b4e5
perkles <maikosilvae@gmail.com>
2022-04-11 15:09:04 -0300

    cliente

--- src/main/java/br/com/titan/estacionamento/model/Cliente.java
+++ src/main/java/br/com/titan/estacionamento/model/Cliente.java
@@ -26,16 +26,6 @@ public class Cliente {
 	@Column(length = 45)
 	private String nome;
 
-	@NotEmpty
-	@CPF(message = "Cpf é obrigatório!")
-	@Column(length = 11, unique = true)
-	private String cpf;
-
-
-	@NotNull
-	@JsonFormat(shape = Shape.STRING, pattern = "dd-MM-yyyy")
-	private LocalDate dataNascimento;
-	
 
 	public Cliente() {
 	}
@@ -56,29 +46,11 @@ public class Cliente {
 		this.nome = nome;
 	}
 
-	public String getCpf() {
-		return cpf;
-	}
-
-	public void setCpf(String cpf) {
-		this.cpf = cpf;
-	}
-
-	public LocalDate getDataNascimento() {
-		return dataNascimento;
-	}
-
-	public void setDataNascimento(LocalDate localDate) {
-		this.dataNascimento = localDate;
-	}
-
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((cpf == null) ? 0 : cpf.hashCode());
-		result = prime * result + ((dataNascimento == null) ? 0 : dataNascimento.hashCode());
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
+				result = prime * result + ((id == null) ? 0 : id.hashCode());
 		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
 		return result;
 	}
@@ -90,27 +62,6 @@ public class Cliente {
 		if (obj == null)
 			return false;
 		if (getClass() != obj.getClass())
-			return false;
-		Cliente other = (Cliente) obj;
-		if (cpf == null) {
-			if (other.cpf != null)
-				return false;
-		} else if (!cpf.equals(other.cpf))
-			return false;
-		if (dataNascimento == null) {
-			if (other.dataNascimento != null)
-				return false;
-		} else if (!dataNascimento.equals(other.dataNascimento))
-			return false;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		if (nome == null) {
-			if (other.nome != null)
-				return false;
-		} else if (!nome.equals(other.nome))
 			return false;
 		return true;
 	}
